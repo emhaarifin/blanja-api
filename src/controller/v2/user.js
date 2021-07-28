4a8e34223472e535feefe535e8e870b5b8cf8d07
Alan Nugraha <101muhammadarifin@gmail.com>
2021-07-28 10:03:26 +0700

    update

--- src/controller/v2/user.js
+++ src/controller/v2/user.js
@@ -168,13 +168,13 @@ module.exports = {
             avatar,
             ...checkUser[0],
           };
+          delete payload.password;
           const token = jwt.sign(payload, process.env.SECRET_KEY, {
             expiresIn: "1h",
           });
           const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN, {
             expiresIn: "24h",
           });
-          delete payload.password;
           payload.token = token;
           payload.refreshToken = refreshToken;
 
